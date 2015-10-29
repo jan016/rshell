dfa3072a4638188941d9dbe17700085aeb4c7dde
jan016 <am9446@gmail.com>
2015-10-29 15:17:50 -0700

    2015-10-29317pm

--- multi_command.sh
+++ multi_command.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+excutable= $1
+argumentList= $2
+connector= $3
+if [ $connector -eq "||" ];
+then
+	
+elif [ $connector -eq "&&" ];
+then
 
-$1 $2 $3
+elif [ $connector -eq ";" ];
+then
 
-
+else
+	echo "connector error"
+fi
